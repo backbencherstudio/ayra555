d40e65e45a58eb6f7a2d2c9b942604e2547302d7
tushar <tushar789imran@gmail.com>
2025-05-22 12:09:21 +0600

    added key workers section

--- app/(client)/life-at-nursery/page.tsx
+++ app/(client)/life-at-nursery/page.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import LifeAtNurseryBanner from './_components/baner'
 import FirstTimeAtNursery from './_components/first-time-at-nursery'
+import KeyWorkers from './_components/key-workers'
 
 export default function LifeAtNursary() {
   return (
     <div>
         <LifeAtNurseryBanner/>
         <FirstTimeAtNursery/>
+        <KeyWorkers/>
     </div>
   )
 }
