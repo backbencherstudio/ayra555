0e48e7f4f36499e7cdde9fa4353c289dbb79de2e
tushar <tushar789imran@gmail.com>
2025-05-22 14:50:38 +0600

    added curriculem aims section

--- app/(client)/curriculum/page.tsx
+++ app/(client)/curriculum/page.tsx
@@ -2,13 +2,15 @@ import React from 'react'
 import CurriculumBanner from './_components/curriculum-banner'
 import Register from './_components/register'
 import BabyGrowApproach from './_components/baby-grow-approach'
+import CurriculumAims from './_components/curriculem-aims'
 
 export default function Curriculum() {
     return (
         <div>
             <CurriculumBanner />
             <Register />
-            <BabyGrowApproach/>
+            <BabyGrowApproach />
+            <CurriculumAims />
         </div>
     )
 }
