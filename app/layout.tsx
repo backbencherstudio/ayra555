1f77ac84dbd159b94064bbc535f18b47e9eb13ed
tushar <tushar789imran@gmail.com>
2025-05-23 17:09:23 +0600

    fevicon upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,42 +12,7 @@ export const metadata: Metadata = {
   title: "Ayra",
   description:
     "Ayra is a platform for creating and sharing AI-powered applications.",
-  icons: {
-    icon: [
-      {
-        rel: 'icon',
-        url: "/favicon-16x16.png",
-        sizes: "16x16",
-        type: "image/png",
-      },
-      {
-        rel: 'icon',
-        url: "/favicon-32x32.png",
-        sizes: "32x32",
-        type: "image/png",
-      },
-      {
-        rel: 'icon',
-        url: "/favicon.ico",
-        sizes: "any",
-      }
-    ],
-    apple: [
-      {
-        url: '/apple-touch-icon.png',
-        sizes: '180x180',
-        type: 'image/png',
-      }
-    ],
-    other: [
-      {
-        rel: 'mask-icon',
-        url: '/safari-pinned-tab.svg',
-        color: '#7CC466'
-      }
-    ]
-  },
-  manifest: '/site.webmanifest',
+  
 };
 
 export default function RootLayout({
@@ -57,6 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <link rel="icon" href="/logo.png" />
       <body className={`${fredoka.variable} font-fredoka antialiased`}>
         <Toaster
           position="top-right"
