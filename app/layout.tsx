50c4be448d6e85fb5cd5765e5c94fb5e8ee2e9c4
tushar-h789 <tushar789imran@gmail.com>
2025-08-20 16:08:32 +0600

    google tag manager script updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,8 +24,44 @@ export default function RootLayout({
       <head>
         <link rel="icon" href="/logo.png" />
         <link rel="canonical" href="https://www.thebabygrow.co.uk/" />
+        
+        {/* Google Tag Manager (GTM) script */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-17382588846"></script>
+        <script>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-17382588846');
+          `}
+        </script>
+        <script>
+          {`
+            (function(w,d,s,l,i){
+              w[l]=w[l]||[];
+              w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});
+              var f=d.getElementsByTagName(s)[0],
+                  j=d.createElement(s),
+                  dl=l!='dataLayer'?'&l='+l:'';
+              j.async=true;
+              j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+              f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-TQ7MBQD9');
+          `}
+        </script>
       </head>
       <body className={`${fredoka.variable} font-fredoka antialiased`}>
+        
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TQ7MBQD9"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
+        
         <Toaster
           position="top-right"
           toastOptions={{
